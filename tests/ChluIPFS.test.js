4133c4e19b9b247e2d211cbc518a285a7c4ae366
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-01-31 17:11:08 +0100

    check for multihash validity and other improvements

--- tests/ChluIPFS.test.js
+++ tests/ChluIPFS.test.js
@@ -4,8 +4,10 @@ const sinon = require('sinon');
 const ChluIPFS = require('../src/ChluIPFS');
 const logger = require('./utils/logger');
 const protons = require('protons');
+const protobuf = protons(require('../src/utils/protobuf'));
 const { getFakeReviewRecord } = require('./utils/protobuf');
 const multihashes = require('multihashes');
+const { isValidMultihash } = require('../src/utils/ipfs');
 
 describe('ChluIPFS', () => {
     it('constructor', () => {
@@ -82,7 +84,6 @@ describe('ChluIPFS', () => {
         const fakeReviewRecord = await getFakeReviewRecord();
         const multihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVPQ'; // not the real multihash
         const multihashBuffer = multihashes.fromB58String(multihash);
-        const protobuf = protons(require('../src/utils/protobuf'));
         const buffer = protobuf.ReviewRecord.encode(fakeReviewRecord);
         const ipfs = {
             object: {
@@ -95,5 +96,13 @@ describe('ChluIPFS', () => {
         expect(ipfs.object.get.args[0][0]).to.deep.equal(multihashBuffer);
         expect(reviewRecord).to.not.be.undefined;
         expect(reviewRecord.chlu_version).to.not.be.undefined;
+    });
+
+    it('recognizes valid and invalid multihashes', async () => {
+        let multihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVPQ';
+        expect(isValidMultihash(multihash)).to.be.true;
+        expect(isValidMultihash(multihashes.fromB58String(multihash))).to.be.true;
+        expect(isValidMultihash('blah')).to.be.false;
+        expect(isValidMultihash({ notEvenA: 'multihash'})).to.be.false;
     });
 });
