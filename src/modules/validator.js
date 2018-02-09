3bbd2326caa0a38f1b86f1804f0fbb9a0392fc7e
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-02-09 11:52:57 +0100

    improved ancestor validation and relevant tests

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -6,9 +6,15 @@ class Validator {
         this.chluIpfs = chluIpfs;
     }
 
-    async validateReviewRecord(reviewRecord) {
-        await this.validateMultihash(reviewRecord, reviewRecord.hash.slice(0));
-        await this.validateHistory(reviewRecord);
+    async validateReviewRecord(reviewRecord, validations = {}) {
+        const v = Object.assign({
+            validateVersion: true,
+            validateMultihash: true,
+            validateHistory: true
+        }, validations);
+        if (v.validateVersion) this.validateVersion(reviewRecord);
+        if (v.validateMultihash) await this.validateMultihash(reviewRecord, reviewRecord.hash.slice(0));
+        if (v.validateHistory) await this.validateHistory(reviewRecord);
     }
 
     async validateMultihash(reviewRecord, expected) {
@@ -20,13 +26,58 @@ class Validator {
 
     async validateHistory(reviewRecord) {
         const history = await this.chluIpfs.reviewRecords.getHistory(reviewRecord);
-        const validations = history.map(async multihash => {
+        if (history.length > 0) {
+            const rr = await this.chluIpfs.reviewRecords.readReviewRecord(history[0]);
+            this.validatePrevious(reviewRecord, rr);
+        }
+        const validations = history.map(async (multihash, i) => {
             const rr = await this.chluIpfs.reviewRecords.readReviewRecord(multihash);
-            await this.validateReviewRecord(rr);
+            await this.validateReviewRecord(rr, { validateHistory: false });
+            if (i !== history.length-1) {
+                const prev = await this.chluIpfs.reviewRecords.readReviewRecord(history[i+1]);
+                this.validatePrevious(rr, prev);
+            }
         });
         await Promise.all(validations);
     }
 
+    validatePrevious(reviewRecord, previousVersion) {
+        // Check that the PoPR was not changed
+        const poprEqual = deepEqual(reviewRecord.popr, previousVersion.popr);
+        if (!poprEqual) {
+            throw new Error('PoPR was changed');
+        }
+        // Check other fields
+        assertFieldsEqual(reviewRecord, previousVersion, [
+            'amount',
+            'currency_symbol',
+            'customer_address',
+            'vendor_address'
+        ]);
+    }
+
+    validateVersion(reviewRecord) {
+        if (reviewRecord.chlu_version !== 0) {
+            throw new Error('Chlu Protocol Version unsupported');
+        }
+    }
+
+}
+
+function deepEqual(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+}
+
+function assertFieldsEqual(a, b, fields) {
+    for (const field of fields) {
+        let equal;
+        if (typeof a[field] === 'object' || typeof b[field] === 'object') {
+            equal = deepEqual(a, b);
+        } else {
+            equal = a[field] === b[field];
+        }
+        if (!equal) throw new Error(field + ' has changed');
+    }
 }
 
 module.exports = Validator;
--- tests/Validator.test.js
+++ tests/Validator.test.js
@@ -8,7 +8,7 @@ const protobuf = protons(require('../src/utils/protobuf'));
 const logger = require('./utils/logger');
 const { getFakeReviewRecord } = require('./utils/protobuf');
 
-describe.only('Validator Module', () => {
+describe('Validator Module', () => {
     let chluIpfs;
 
     beforeEach(() => {
@@ -70,14 +70,41 @@ describe.only('Validator Module', () => {
     });
 
     it('validates ancestors', async () => {
-        sinon.spy(chluIpfs.validator, 'validateReviewRecord');
-        chluIpfs.validator.validateMultihash = sinon.stub().resolves();
-        let reviewRecord = await getFakeReviewRecord();
-        reviewRecord.previous_version_multihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP1';
-        let originalReviewRecord = Object.assign({}, reviewRecord);
-        originalReviewRecord.previous_version_multihash = '';
-        chluIpfs.reviewRecords.readReviewRecord = sinon.stub().resolves(originalReviewRecord);
-        await chluIpfs.validator.validateReviewRecord(reviewRecord);
-        expect(chluIpfs.reviewRecords.readReviewRecord.calledWith(reviewRecord.previous_version_multihash)).to.be.true;
+        const chluIpfs = new ChluIPFS({ type: ChluIPFS.types.customer, enablePersistence: false, logger: logger('Customer') });
+        const reviewRecord = await getFakeReviewRecord();
+        reviewRecord.rating = 1;
+        const reviewRecord2 = await getFakeReviewRecord();
+        reviewRecord2.rating = 2;
+        reviewRecord2.previous_version_multihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP1';
+        const reviewRecord3 = await getFakeReviewRecord();
+        reviewRecord3.rating = 3;
+        reviewRecord3.previous_version_multihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP2';
+        let map = {
+            'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP1': reviewRecord,
+            'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP2': reviewRecord2
+        };
+        chluIpfs.reviewRecords.getReviewRecord = sinon.stub().callsFake(async multihash => {
+            return map[multihash];
+        });
+        chluIpfs.validator.validateMultihash = sinon.stub().resolves(); // disable since we don't need it for this test
+        sinon.spy(chluIpfs.validator, 'validatePrevious');
+        await chluIpfs.validator.validateReviewRecord(reviewRecord3);
+        expect(chluIpfs.validator.validatePrevious.calledWith(reviewRecord3, reviewRecord2)).to.be.true;
+        expect(chluIpfs.validator.validatePrevious.calledWith(reviewRecord2, reviewRecord)).to.be.true;
+        expect(chluIpfs.validator.validatePrevious.callCount).to.equal(2);
+        expect(chluIpfs.reviewRecords.getReviewRecord.calledWith(reviewRecord3.previous_version_multihash)).to.be.true;
+        // Test failure cases
+        chluIpfs.validator.validateMultihash.reset();
+        chluIpfs.validator.validatePrevious.reset();
+        chluIpfs.validator.validatePrevious.reset();
+        // Change review2 to be an invalid update
+        reviewRecord2.customer_address = 'changed';
+        let error;
+        try {
+            await chluIpfs.validator.validateReviewRecord(reviewRecord3);
+        } catch (err) {
+            error = err;
+        }
+        expect(error.message).to.equal('customer_address has changed');
     });
 });
