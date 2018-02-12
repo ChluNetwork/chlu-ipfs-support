e2889989c290ca79adcc0f37046837481784eaf4
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-02-12 13:55:32 +0100

    only pin valid review records

--- src/modules/room.js
+++ src/modules/room.js
@@ -81,12 +81,16 @@ class Room {
                 const isOrbitDb = obj.type === constants.eventTypes.customerReviews || obj.type === constants.eventTypes.updatedReview;
                 // handle ReviewRecord: pin hash
                 if (obj.type === constants.eventTypes.wroteReviewRecord && typeof obj.multihash === 'string') {
-                    this.chluIpfs.logger.info('Pinning ReviewRecord ' + obj.multihash);
+                    this.chluIpfs.logger.info('Reading and Pinning ReviewRecord ' + obj.multihash);
                     try {
+                        // Read review record first. This caches the content, the history, and throws if it's not valid
+                        this.chluIpfs.logger.debug('Reading and validating ReviewRecord ' + obj.multihash);
+                        await this.chluIpfs.readReviewRecord(obj.multihash);
+                        this.chluIpfs.logger.debug('Pinning validated ReviewRecord ' + obj.multihash);
                         await this.chluIpfs.pinning.pin(obj.multihash);
-                        this.chluIpfs.logger.info('Pinned ReviewRecord ' + obj.multihash);
+                        this.chluIpfs.logger.info('Validated and Pinned ReviewRecord ' + obj.multihash);
                     } catch(exception){
-                        this.chluIpfs.logger.error('Pin Error: ' + exception.message);
+                        this.chluIpfs.logger.error('Pinning failed due to Error: ' + exception.message);
                     }
                 } else if (isOrbitDb && typeof obj.address === 'string') {
                     // handle OrbitDB: replicate
