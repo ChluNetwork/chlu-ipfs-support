af96f7d308c28ba6bf4e7c7bb933f17e28149fb7
Enrico Fasoli <fazo96@users.noreply.github.com>
2018-03-12 15:06:33 +0100

    Fixes for vendor module
    * fix small bug with storing public keys in IPFS
    
    * fix broken pin wrapper
    
    * new vendor tests

--- tests/Vendor.test.js
+++ tests/Vendor.test.js
@@ -5,6 +5,8 @@ const ChluIPFS = require('../src/ChluIPFS');
 const logger = require('./utils/logger');
 const { getFakeReviewRecord } = require('./utils/protobuf');
 const { ECPair } = require('bitcoinjs-lib');
+const DAGNode = require('ipld-dag-pb').DAGNode;
+const { isValidMultihash } = require('../src/utils/ipfs');
 
 describe('Vendor Module', () => {
     let chluIpfs, keyPair, map, pubKeyMultihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP1';
@@ -25,6 +27,16 @@ describe('Vendor Module', () => {
         chluIpfs.ipfsUtils.get = sinon.stub().callsFake(async multihash => {
             return map[multihash];
         });
+        chluIpfs.ipfsUtils.put = sinon.stub().callsFake(async data => {
+            const buf = Buffer.from(data);
+            const multihash = await new Promise((resolve, reject) => {
+                DAGNode.create(buf, [], (err, dagNode) => {
+                    if (err) reject(err); else resolve(dagNode.toJSON().multihash);
+                });
+            });
+            map[multihash] = buf;
+            return multihash;
+        });
     });
 
     afterEach(() => {
@@ -32,7 +44,7 @@ describe('Vendor Module', () => {
         map = null;
     });
 
-    it('signs and verifies', async () => {
+    it('signs and verifies PoPRs', async () => {
         const reviewRecord = await getFakeReviewRecord();
         await chluIpfs.vendor.signPoPR(reviewRecord.popr, keyPair);
         const verification = await chluIpfs.vendor.verifyPoPR(reviewRecord.popr, pubKeyMultihash);
@@ -41,5 +53,17 @@ describe('Vendor Module', () => {
         reviewRecord.popr.amount = 200;
         const verificationToFail = await chluIpfs.vendor.verifyPoPR(reviewRecord.popr, pubKeyMultihash);
         expect(verificationToFail).to.be.false;
+    });
+
+    it('retrieves public keys', async () => {
+        const buf = await chluIpfs.vendor.getPublicKey(pubKeyMultihash);
+        expect(buf).to.deep.equal(keyPair.getPublicKeyBuffer());
+    });
+
+    it('stores public keys', async () => {
+        const keyPair = ECPair.makeRandom();
+        const multihash = await chluIpfs.vendor.storePublicKey(keyPair.getPublicKeyBuffer());
+        expect(multihash).to.be.a('string');
+        expect(isValidMultihash(multihash)).to.be.true;
     });
 });
