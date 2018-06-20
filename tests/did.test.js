224676de32482482446c5dbaa7867de15bd4eba2
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-20 14:51:35 +0200

    finish the DID module tests

--- tests/did.test.js
+++ tests/did.test.js
@@ -1,125 +1,115 @@
 const expect = require('chai').expect;
-const sinon = require('sinon');
+const sinon = require('sinon')
 
 const ChluIPFS = require('../src/ChluIPFS');
 const logger = require('./utils/logger');
+const cryptoUtils = require('./utils/crypto')
 const { getFakeReviewRecord } = require('./utils/protobuf');
-const { ECPair } = require('bitcoinjs-lib');
-const DAGNode = require('ipld-dag-pb').DAGNode;
-const { isValidMultihash } = require('../src/utils/ipfs');
+const ipfsUtilsStub = require('./utils/ipfsUtilsStub')
 
-describe.skip('DID Module', () => {
-    let chluIpfs, keyPair, map, pubKeyMultihash = 'QmQ6vGTgqjec2thBj5skqfPUZcsSuPAbPS7XvkqaYNQVP1';
+describe('DID Module', () => {
+    let chluIpfs, exampleDID, exampleDIDID, makeDID
 
-    before(() => {
-        keyPair = ECPair.makeRandom();
-    });
-
-    beforeEach(() => {
+    beforeEach(async () => {
         chluIpfs = new ChluIPFS({
             type: ChluIPFS.types.vendor,
             enablePersistence: false,
             cache: { enabled: false },
             logger: logger('Vendor')
         });
-        map = {
-            [pubKeyMultihash]: keyPair.getPublicKeyBuffer()
-        };
-        chluIpfs.ipfsUtils.get = sinon.stub().callsFake(async multihash => {
-            return map[multihash];
-        });
-        chluIpfs.ipfsUtils.put = sinon.stub().callsFake(async data => {
-            const buf = Buffer.from(data);
-            const multihash = await new Promise((resolve, reject) => {
-                DAGNode.create(buf, [], (err, dagNode) => {
-                    if (err) reject(err); else resolve(dagNode.toJSON().multihash);
-                });
-            });
-            map[multihash] = buf;
-            return multihash;
-        });
+        // Set up all the stubs of orbit and IPFS so that
+        // the DID is returned properly when calling stubbed OrbitDB
+        // and stubbed IPFS
+        const utils = cryptoUtils(chluIpfs)
+        makeDID = utils.makeDID
+        exampleDID = await makeDID()
+        exampleDIDID = exampleDID.publicDidDocument.id
+        const didStore = utils.buildDIDMap([exampleDID])
+        const fakeStore = {
+            'Qmabc': Buffer.from(JSON.stringify(exampleDID.publicDidDocument))
+        }
+        // stubs
+        chluIpfs.ipfsUtils = ipfsUtilsStub(fakeStore)
+        chluIpfs.orbitDb.getDID = sinon.stub().callsFake(async id => didStore[id] ? 'Qmabc' : null)
+        chluIpfs.orbitDb.putDID = sinon.stub().callsFake(did => {
+            didStore[did.id] = did
+        })
     });
 
     afterEach(() => {
         chluIpfs = null;
-        map = null;
     });
 
-    it('publish')
-    it('generate')
-    it('getDID')
 
-    it('signs Review Records', async () => {
-        async function verifyRR(rr, pubKeyMultihash) {
+    it('signs and verifies Review Records', async () => {
+        await chluIpfs.did.start()
+        async function verifyRR(rr) {
             const hashed = await chluIpfs.reviewRecords.hashReviewRecord(rr);
-            return await chluIpfs.crypto.verifyMultihash(
-                pubKeyMultihash,
+            return await chluIpfs.did.verifyMultihash(
+                chluIpfs.did.didId,
                 hashed.hash,
                 hashed.signature
             );
         }
         let reviewRecord = await getFakeReviewRecord();
-        reviewRecord = await chluIpfs.crypto.signReviewRecord(reviewRecord, keyPair);
-        const verification = await verifyRR(reviewRecord, pubKeyMultihash);
+        reviewRecord = await chluIpfs.did.signReviewRecord(reviewRecord);
+        const verification = await verifyRR(reviewRecord);
         expect(verification).to.be.true;
         // Test failure case: change a field and validate again
         reviewRecord.review_text = 'Hellooooo';
-        const verificationToFail = await verifyRR(reviewRecord, pubKeyMultihash);
-        expect(verificationToFail).to.be.false;
-    });
-
-    it('signs PoPRs', async () => {
-        async function verifyPoPR(popr, pubKeyMultihash) {
-            const hashed = await chluIpfs.reviewRecords.hashPoPR(popr);
-            return await chluIpfs.crypto.verifyMultihash(
-                pubKeyMultihash,
-                hashed.hash,
-                hashed.signature
-            );
-        }
-        const reviewRecord = await getFakeReviewRecord();
-        reviewRecord.popr = await chluIpfs.crypto.signPoPR(reviewRecord.popr, keyPair);
-        const verification = await verifyPoPR(reviewRecord.popr, pubKeyMultihash);
-        expect(verification).to.be.true;
-        // Test failure case: change a field and validate again
-        reviewRecord.popr.amount = 200;
-        const verificationToFail = await verifyPoPR(reviewRecord.popr, pubKeyMultihash);
+        const verificationToFail = await verifyRR(reviewRecord);
         expect(verificationToFail).to.be.false;
     });
 
     it('retrieves DID by ID', async () => {
-        const buf = await chluIpfs.crypto.getPublicKey(pubKeyMultihash);
-        expect(buf).to.deep.equal(keyPair.getPublicKeyBuffer());
+        const did = await chluIpfs.did.getDID(exampleDIDID);
+        expect(did).to.deep.equal(exampleDID.publicDidDocument);
     });
 
     it('publishes DID Public Document', async () => {
-        const keyPair = ECPair.makeRandom();
-        const multihash = await chluIpfs.crypto.storePublicKey(keyPair.getPublicKeyBuffer());
-        expect(multihash).to.be.a('string');
-        expect(isValidMultihash(multihash)).to.be.true;
+        await chluIpfs.did.generate()
+        await chluIpfs.did.publish() 
+        expect(chluIpfs.orbitDb.getDID.calledWith(chluIpfs.did.didId)).to.be.true
+        // need to use await here to unpack value from promise, even though the promise
+        // is already resolved because it is awaited by the publish() call
+        const multihash = await chluIpfs.ipfsUtils.putJSON.returnValues[0]
+        const existingMultihash = await chluIpfs.orbitDb.getDID.returnValues[0]
+        expect(existingMultihash).to.be.null
+        expect(multihash).to.be.a('string')
+        expect(chluIpfs.orbitDb.putDID.calledWith(chluIpfs.did.didId, multihash)).to.be.true
     });
 
     it('generates DID', async () => {
-        const keyPair = await chluIpfs.crypto.generateKeyPair();
-        expect(chluIpfs.ipfsUtils.put.calledWith(keyPair.getPublicKeyBuffer())).to.be.true;
-        expect(keyPair instanceof ECPair).to.be.true;
-        expect(chluIpfs.crypto.keyPair).to.equal(keyPair);
-        expect(isValidMultihash(chluIpfs.crypto.pubKeyMultihash)).to.be.true;
+        expect(chluIpfs.did.didId).to.be.null
+        expect(chluIpfs.did.publicDidDocument).to.be.null
+        expect(chluIpfs.did.privateKeyBase58).to.be.null
+        await chluIpfs.did.start()
+        expect(chluIpfs.did.didId).to.match(/^did:chlu:/)
+        expect(chluIpfs.did.publicDidDocument).to.be.an('object')
+        expect(chluIpfs.did.privateKeyBase58).to.be.a('string')
+        expect(chluIpfs.did.publicDidDocument.id).to.equal(chluIpfs.did.didId)
     });
 
     it('imports DID', async () => {
-        const keyPair = ECPair.makeRandom();
-        const imported = await chluIpfs.crypto.importKeyPair(keyPair.toWIF());
-        expect(imported instanceof ECPair).to.be.true;
-        expect(chluIpfs.ipfsUtils.put.calledWith(keyPair.getPublicKeyBuffer())).to.be.true;
-        expect(chluIpfs.crypto.keyPair.toWIF()).to.equal(keyPair.toWIF());
-        expect(isValidMultihash(chluIpfs.crypto.pubKeyMultihash)).to.be.true;
+        expect(chluIpfs.did.didId).to.be.null
+        expect(chluIpfs.did.publicDidDocument).to.be.null
+        expect(chluIpfs.did.privateKeyBase58).to.be.null
+        const did = await makeDID()
+        await chluIpfs.did.import(did)
+        expect(chluIpfs.did.didId).to.match(/^did:chlu:/)
+        expect(chluIpfs.did.publicDidDocument).to.be.an('object')
+        expect(chluIpfs.did.privateKeyBase58).to.be.a('string')
+        expect(chluIpfs.did.publicDidDocument.id).to.equal(chluIpfs.did.didId)
     });
 
     it('exports DID', async () => {
-        const keyPair = ECPair.makeRandom();
-        chluIpfs.crypto.keyPair = keyPair;
-        const exported = await chluIpfs.crypto.exportKeyPair();
-        expect(exported).to.equal(keyPair.toWIF());
+        await chluIpfs.did.generate()
+        expect(chluIpfs.did.didId).to.match(/^did:chlu:/)
+        expect(chluIpfs.did.publicDidDocument).to.be.an('object')
+        expect(chluIpfs.did.privateKeyBase58).to.be.a('string')
+        expect(chluIpfs.did.publicDidDocument.id).to.equal(chluIpfs.did.didId)
+        const did = await chluIpfs.did.export()
+        expect(chluIpfs.did.publicDidDocument).to.deep.equal(did.publicDidDocument)
+        expect(chluIpfs.did.privateKeyBase58).to.deep.equal(did.privateKeyBase58)
     });
 });
--- tests/utils/ipfsUtilsStub.js
+++ tests/utils/ipfsUtilsStub.js
@@ -2,19 +2,38 @@ const sinon = require('sinon');
 const DAGNode = require('ipld-dag-pb').DAGNode;
 
 module.exports = function (fakeStore) {
+    const get = sinon.stub().callsFake(async m => fakeStore[m])
+    const getJSON = sinon.stub().callsFake(async m => {
+        const buf = fakeStore[m]
+        if (buf) {
+            const str = buf.toString()
+            return JSON.parse(str)
+        } else {
+            return null
+        }
+    })
+    const putRaw = async data => {
+        const buf = Buffer.from(data);
+        const multihash = await new Promise((resolve, reject) => {
+            DAGNode.create(buf, [], (err, dagNode) => {
+                if (err) reject(err); else resolve(dagNode.toJSON().multihash);
+            });
+        });
+        fakeStore[multihash] = buf;
+        return multihash;
+    }
+    const put = sinon.stub().callsFake(putRaw)
+    const putJSON = sinon.stub().callsFake(async data => {
+        let arg = data
+        if (typeof arg !== 'string') arg = JSON.stringify(arg)
+        return putRaw(arg)
+    })
     return {
         id: sinon.stub().resolves('myId'),
-        get: sinon.stub().callsFake(async m => fakeStore[m]),
-        put: sinon.stub().callsFake(async data => {
-            const buf = Buffer.from(data);
-            const multihash = await new Promise((resolve, reject) => {
-                DAGNode.create(buf, [], (err, dagNode) => {
-                    if (err) reject(err); else resolve(dagNode.toJSON().multihash);
-                });
-            });
-            fakeStore[multihash] = buf;
-            return multihash;
-        }),
+        get,
+        getJSON,
+        put,
+        putJSON,
         storeDAGNode: sinon.stub().callsFake(async dagNode => {
             const data = dagNode.toJSON();
             fakeStore[data.multihash] = data.data;
