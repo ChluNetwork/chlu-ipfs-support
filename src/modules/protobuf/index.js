d65e14760a5f11ec227bce4b71ef71f499e801c8
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-06-28 14:50:40 +0200

    WIP google-protobuf integration. Blocked by google/protobuf#1591

--- src/modules/protobuf/index.js
+++ src/modules/protobuf/index.js
@@ -1,12 +1,42 @@
-const protons = require('protons')
-const sources = require('./sources')
+const proto = require('./compiled/reviewrecord_pb')
 
 class ChluProtobuf {
     constructor() {
-        this.proto = protons(sources)
-        this.ReviewRecord = this.proto.ReviewRecord
-        this.PoPR = this.proto.PoPR
+        this.ReviewRecord = {
+            encode: encoder(proto.ReviewRecord),
+            decode: decoder(proto.ReviewRecord)
+        }
+        this.PoPR = {
+            encode: encoder(proto.PoPR),
+            decoder: decoder(proto.PoPR)
+        }
     }
+}
+
+function encoder(proto) {
+    return data => {
+        console.log(data)
+        const obj = new proto(data)
+        const array = obj.serializeBinary() // Uint8Array
+        return Buffer.from(array)
+    }
+}
+
+function decoder(proto) {
+    return data => {
+        const obj = proto.deserializeBinary(data)
+        const res = obj.toObject()
+        console.log(res)
+    }
+}
+
+function uint16ArrayToUint8Array(input) {
+    const output = new Uint8Array()
+    for (let i = 0; i < input.length; i++) {
+        output.push(input[i] & 0xff)
+        output.push(input[i] >> 8)
+    }
+    return output
 }
 
 module.exports = ChluProtobuf
