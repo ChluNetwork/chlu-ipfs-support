5f03a225f7e60636e0e653de1417bb270d2daf89
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-23 14:45:15 +0200

    add isPinned API and "broadcast" option to Pin API

--- src/modules/pinning.js
+++ src/modules/pinning.js
@@ -1,5 +1,6 @@
 const constants = require('../constants');
 const IPFSUtils = require('../utils/ipfs');
+const { find, isEmpty } = require('lodash')
 
 class Pinning {
 
@@ -7,26 +8,45 @@ class Pinning {
         this.chluIpfs = chluIpfs;
     }
 
-    async pin(multihash){
+    async pin(multihash, broadcast = true){
         IPFSUtils.validateMultihash(multihash);
+        this.chluIpfs.logger.debug(`pin ${multihash} (broadcast: ${broadcast}) => ...`)
         // TODO: check that the multihash evaluates to valid Chlu data
         // broadcast start of pin process
-        await this.chluIpfs.room.broadcast({ type: constants.eventTypes.pinning, multihash });
+        if (broadcast) await this.chluIpfs.room.broadcast({ type: constants.eventTypes.pinning, multihash });
         this.chluIpfs.events.emit('chlu-ipfs/pinning', multihash);
         try {
-            if (this.chluIpfs.ipfs.pin) {
-                await this.chluIpfs.ipfs.pin.add(multihash, { recursive: true });
-            } else {
-                // TODO: Chlu service node need to be able to pin, so we should support using go-ipfs
-                await this.chluIpfs.ipfsUtils.get(multihash);
-            }
+            await this.chluIpfs.ipfs.pin.add(multihash, { recursive: true });
             this.chluIpfs.events.emit('chlu-ipfs/pinned', multihash);
             // broadcast successful pin
-            await this.chluIpfs.room.broadcast({ type: constants.eventTypes.pinned, multihash });
+            if (broadcast) await this.chluIpfs.room.broadcast({ type: constants.eventTypes.pinned, multihash });
+            this.chluIpfs.logger.debug(`pin ${multihash} (broadcast: ${broadcast}) => OK`)
         } catch (error) {
-            this.chluIpfs.logger.error('IPFS Pin Error: ' + error.message);
+            this.chluIpfs.logger.error(`pin ${multihash} (broadcast: ${broadcast}) => ERROR`)
+            console.log(error)
             this.chluIpfs.events.emit('chlu-ipfs/pin/error', { multihash, error });
             this.chluIpfs.events.emit('chlu-ipfs/error', error);
+        }
+    }
+
+    async isPinned(multihash) {
+        IPFSUtils.validateMultihash(multihash)
+        try {
+            this.chluIpfs.logger.debug(`isPinned ${multihash} => ...`)
+            const pinset = await this.chluIpfs.ipfs.pin.ls(multihash)
+            const pinned = !isEmpty(find(pinset, {
+                hash: multihash
+            }))
+            this.chluIpfs.logger.debug(`isPinned ${multihash} => OK ${pinned}`)
+            return pinned
+        } catch(error) {
+            if (error.message === `Path ${multihash} is not pinned`) {
+                // TODO: this is weird, why does ls not just return [] when data is not pinned?
+                this.chluIpfs.logger.debug(`isPinned ${multihash} => OK false (due to "not pinned" error from IPFS)`)
+                return false
+            }
+            this.chluIpfs.logger.error(`isPinned ${multihash} => ERROR`)
+            throw error
         }
     }
 }
